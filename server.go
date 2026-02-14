390df49a337cc51a38697648a2345be4b2a02bdc
Clément <33451311+clmntgbr@users.noreply.github.com>
2026-02-14 11:12:44 +0100

    chore(server): remove unused imports and adjust formatting

--- server.go
+++ server.go
@@ -5,20 +5,17 @@ import (
 
 	"uplink-go/config"
 
-  "github.com/gofiber/fiber/v3"
+    "github.com/gofiber/fiber/v3"
 	"github.com/gofiber/fiber/v3/middleware/healthcheck"
 	"github.com/gofiber/fiber/v3/middleware/helmet"
 	"github.com/gofiber/fiber/v3/middleware/logger"
-	"github.com/joho/godotenv"
-	"os"
-	"strings"
 )
 
 func main() {
-		cfg := config.Load()
+    cfg := config.Load()
 
-		db := config.ConnectDatabase(cfg)
-		config.AutoMigrate(db)
+    db := config.ConnectDatabase(cfg)
+    config.AutoMigrate(db)
 	
     app := fiber.New(fiber.Config{
         CaseSensitive: true,
@@ -27,17 +24,17 @@ func main() {
         AppName:       "Uplink Go Server v1.0.0",
     })
 
-		app.Use(helmet.New())
+    app.Use(helmet.New())
 
-		app.Use(logger.New(logger.Config{
-				Format: "[${ip}]:${port} ${status} - ${method} ${path}\n",
-		}))
+    app.Use(logger.New(logger.Config{
+        Format: "[${ip}]:${port} ${status} - ${method} ${path}\n",
+    }))
 
-		app.Get(healthcheck.LivenessEndpoint, healthcheck.New())
-		app.Get(healthcheck.ReadinessEndpoint, healthcheck.New())
-		app.Get(healthcheck.StartupEndpoint, healthcheck.New())
-		
-		// api := app.Group("/api")
+    app.Get(healthcheck.LivenessEndpoint, healthcheck.New())
+    app.Get(healthcheck.ReadinessEndpoint, healthcheck.New())
+    app.Get(healthcheck.StartupEndpoint, healthcheck.New())
+    
+    // api := app.Group("/api")
 
-		log.Fatal(app.Listen(":3000"))
+    log.Fatal(app.Listen(":3000"))
 }
